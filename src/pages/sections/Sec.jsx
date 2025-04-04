efe83857763098aea13adc08ec003fe2bef23149
vxlcdm <dspnoida420@gmail.com>
2025-04-04 07:52:54 +0530

    added pages almost all

---
+++ src/pages/sections/Sec.jsx
@@ -0,0 +1,422 @@
+import React from 'react';
+import {Container} from "@mui/material";
+import { useState } from "react";
+import {
+  Box,
+  Typography,
+  Grid,
+  Paper,
+} from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import ForumIcon from "@mui/icons-material/Forum";
+import PermMediaIcon from "@mui/icons-material/PermMedia";
+import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
+
+
+
+
+
+
+const Sec=()=>{
+    return(<>
+
+                {/* Hero Section */}
+      <Box
+        sx={{
+          height: "100vh",
+          backgroundImage: `url("https://picsum.photos/seed/icdwe3fvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+           backgroundAttachment:"fixed",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+       <Typography 
+        variant="h4" 
+        fontWeight="bold" 
+        color="black"
+        sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem", lg: "4.3rem" } }}
+        >
+        Slideshow Sections
+        </Typography>
+        </Box>
+
+
+
+
+        <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+             Hero Slideshow
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdvdwe3fvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+
+
+
+
+
+        <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+             Hero Slideshow - Image Only
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdvcdwe3fvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+
+         <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+             Hero Panels
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdvvcdwe3fvddesvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+
+        <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+             Hero Banner
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdvvcdswe3dfvcddvesvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+         <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+             Image Slideshow
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdvvcdswe3fbvncddesnvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+
+
+        <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+             Image Slideshow - Thumbnails
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdrxvvcdswe3fvcddesvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+
+        <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+             Image Slideshow - Content Sidebar
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdvvcdswe3fvcfbdbnzdgrbzzzddesvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+        <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+             Image Slideshow - Map Sidebar
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdvvcdswe3fvcbbbbbbbzbbbbrddesvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+
+        <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+             Image Carousel
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/iddcdfdvvcdswe3fvbcddesvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+
+
+
+        <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+            Image Carousel - Navigation
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdvvcdswe3fvcdvbdesvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+
+
+         <Box>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10 }}
+             >
+            Visual Navigation
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdvvcdswe3zdfdffzfvcddesvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+
+
+
+          <Box sx={{mb:12}}>
+            <Typography 
+             variant="h4" 
+             fontWeight="bold" 
+             color="Black"
+             sx={{ fontSize: { xs: "2.2rem", sm: "2.9rem", md: "3.6rem",
+                 lg: "2.2rem" }, textAlign:"center", p:10,  }}
+             >
+            Visual Navigation
+             </Typography>
+        <Box
+        sx={{
+          height: "75vh",
+          backgroundImage: `url("https://picsum.photos/seed/idcdfdvvcdswe3dfbbfhilhbgu8ig;j;eddfcrdczewfvcddesvdah/1920/1080")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+        
+          alignItems: "center",
+          justifyContent: "center",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        </Box>
+        </Box>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </>)
+}
+export default Sec;
+
